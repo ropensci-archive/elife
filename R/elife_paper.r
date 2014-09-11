a9c70841afe9f339e805fa40e818d8b02ec1f105
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-11 16:59:25 -0400

    added raw text output option to elife_paper, and white space diffs, sorry

--- R/elife_paper.r
+++ R/elife_paper.r
@@ -16,47 +16,50 @@ elife_paper <- function(doi = NULL, what='xml')
 {
 	url <- sprintf("http://elife.elifesciences.org/elife-source-xml/%s", doi)
 	tt <- content(GET(url), as="text")
-	out <- xmlParse(tt)
   
-  what <- match.arg(what, c('xml','all','abstract','exec_summary','introduction','methods','results','discussion'))
+  what <- match.arg(what, c('text','xml','all','abstract','exec_summary','introduction','methods','results','discussion'))
   
-  if(what=='xml') { 
-    out 
-  } else if(what=='all') { 
-    message("to be fixed")
-#     tmp <- xpathApply(out, "//p", fun=xmlValue)
-#     tmp[!vapply(tmp, function(z) grepl("DOI", z), TRUE)]
-  } else if(what=='abstract'){  
-    abstract <- xpathApply(out, "//abstract[@hwp:id='abstract-1']/p", fun=xmlValue)
-    extract_section(abstract)
-  } else if(what=='exec_summary'){
-    exec_summary <- xpathApply(out, "//abstract[@hwp:id='abstract-2']/p", fun=xmlValue)
-    extract_section(exec_summary)
-  } else if(what=='introduction'){
-    intro <- xpathApply(out, "//body/sec[@sec-type='intro']/p", fun=xmlValue)
-    extract_section(intro)
-  } else if(what=='results'){
-    ## FIX ME
-    message("to be fixed")
-    # 	xpathApply(out, "//body/sec[@sec-type='results']//p", fun=xmlValue)
-#     resultsnodes_title <- getNodeSet(out, "//body/sec[@sec-type='results']/sec/title")
-#     resultsnodes_p <- getNodeSet(out, "//body/sec[@sec-type='results']/sec/p")
-#     res_titles <- sapply(resultsnodes_title, xmlValue)
-#     res_p <- sapply(resultsnodes_p, xmlValue)
-#     names(res_p) <- res_titles
-  } else if(what=='methods'){
-    ## FIX ME
-    message("to be fixed")
-#     disc <- 
-#       xpathApply(out, "//body/sec[@sec-type='methods']//p", fun=xmlValue)
-#     res <- list()
-#     res$doi <- disc[grepl("DOI", disc)][[1]]
-#     res$text <- disc[!grepl("DOI", disc)]
-#     res
-  } else if(what=='discussion'){
-    disc <- xpathApply(out, "//body/sec[@sec-type='discussion']//p", fun=xmlValue)
-    extract_section(disc)
-  } 
+	if(what=='text'){ tt } else {
+	  out <- xmlParse(tt)
+	  
+	  if(what=='xml') { 
+	    out 
+	  } else if(what=='all') { 
+	    message("to be fixed")
+	    #     tmp <- xpathApply(out, "//p", fun=xmlValue)
+	    #     tmp[!vapply(tmp, function(z) grepl("DOI", z), TRUE)]
+	  } else if(what=='abstract'){  
+	    abstract <- xpathApply(out, "//abstract[@hwp:id='abstract-1']/p", fun=xmlValue)
+	    extract_section(abstract)
+	  } else if(what=='exec_summary'){
+	    exec_summary <- xpathApply(out, "//abstract[@hwp:id='abstract-2']/p", fun=xmlValue)
+	    extract_section(exec_summary)
+	  } else if(what=='introduction'){
+	    intro <- xpathApply(out, "//body/sec[@sec-type='intro']/p", fun=xmlValue)
+	    extract_section(intro)
+	  } else if(what=='results'){
+	    ## FIX ME
+	    message("to be fixed")
+	    # 	xpathApply(out, "//body/sec[@sec-type='results']//p", fun=xmlValue)
+	    #     resultsnodes_title <- getNodeSet(out, "//body/sec[@sec-type='results']/sec/title")
+	    #     resultsnodes_p <- getNodeSet(out, "//body/sec[@sec-type='results']/sec/p")
+	    #     res_titles <- sapply(resultsnodes_title, xmlValue)
+	    #     res_p <- sapply(resultsnodes_p, xmlValue)
+	    #     names(res_p) <- res_titles
+	  } else if(what=='methods'){
+	    ## FIX ME
+	    message("to be fixed")
+	    #     disc <- 
+	    #       xpathApply(out, "//body/sec[@sec-type='methods']//p", fun=xmlValue)
+	    #     res <- list()
+	    #     res$doi <- disc[grepl("DOI", disc)][[1]]
+	    #     res$text <- disc[!grepl("DOI", disc)]
+	    #     res
+	  } else if(what=='discussion'){
+	    disc <- xpathApply(out, "//body/sec[@sec-type='discussion']//p", fun=xmlValue)
+	    extract_section(disc)
+	  } 
+	}
 }
 
 extract_section <- function(x){
